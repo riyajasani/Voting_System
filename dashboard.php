8abe66b77b1effea1b50148ecb72d40f465d5bbb
riyajasani <129930943+riyajasani@users.noreply.github.com>
2023-04-15 15:50:26 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voting System -Dashboard</title>
+</head>
+<body class="bg-secondary text-light">
+    <div class="container my-5">
+<a href="../"><button class="btn btn-dark text-light px-3">Back</button></a>
+<a href="logout.php"><button class="btn btn-dark text-light px-3">Logout</button></a>
+<h1 class="my-3">Voting System</h1>
+<div class="row my-5">
+    <div class="col-md-7">
+    <!-- groups -->
+    <div class="row">
+        <div class="col-md-4"></div>
+            <img src="" alt="Group image">
+        </div>
+       <div class="col-md-8"></div>
+            <strong class="text-dark h5">Group name:</strong><br>
+            <strong class="text-dark h5">Votes:</strong><br>
+        </div>
+    </div>
+    <hr>
+</div>
+<div class="col-md-5">
+    <!-- user profile -->
+    <img src="" alt="User image">
+    <br>
+    <br>
+    <strong class="text-dark h5">Name:</strong><br><br>
+    <strong class="text-dark h5">Mobile:</strong><br><br>
+    <strong class="text-dark h5">Status:</strong><br><br>
+</div>
+</body>
+</html>
